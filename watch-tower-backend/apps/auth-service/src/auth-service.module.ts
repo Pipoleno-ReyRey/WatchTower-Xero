56aee327315751c4b1f601e8590cc36db1195c41
Reynaldo A. Suero Delmonte <reysuero2011@gmail.com>
2026-02-18 23:50:09 -0400

    add secrets to jwt and generate token service implementation

--- watch-tower-backend/apps/auth-service/src/auth-service.module.ts
+++ watch-tower-backend/apps/auth-service/src/auth-service.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthServiceController } from './auth-service.controller';
 import { AuthService} from './auth-service.service';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.TOKEN_SECRET,
+      signOptions: {expiresIn: "1h"}
+    })
+  ],
   controllers: [AuthServiceController],
   providers: [AuthService],
 })
--- watch-tower-backend/apps/auth-service/src/auth-service.service.ts
+++ watch-tower-backend/apps/auth-service/src/auth-service.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from 'core/dtos/login.dto';
+import { UserDto } from 'core/dtos/user.dto';
 
 @Injectable()
 export class AuthService{
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private jwt: JwtService
+  ){}
+
+  async generateToken(user: UserDto): Promise<string> {
+    let payload = {
+      userName: user.userName,
+      email: user.email,
+      roles: user.role
+    }
+
+    return this.jwt.sign(payload);
   }
 }
