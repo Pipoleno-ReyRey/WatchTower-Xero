05822916c71f681ac666ec188a536643d79766d3
Reynaldo A. Suero Delmonte <reysuero2011@gmail.com>
2026-03-07 22:33:33 -0400

    enable cors

--- watch-tower-backend/apps/gateway/src/main.ts
+++ watch-tower-backend/apps/gateway/src/main.ts
@@ -3,6 +3,7 @@ import { GatewayModule } from './gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
+  app.enableCors();
   await app.listen(process.env.GATEWAY_PORT ?? 3000);
 }
 bootstrap();
